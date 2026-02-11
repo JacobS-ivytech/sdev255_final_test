207293053c610c775ac3c9af39e64106ab6da48a
bbeck2417 <billys.beck@gmail.com>
2026-02-10 20:16:39 -0500

    added form

--- src/app/page.js
+++ src/app/page.js
@@ -2,64 +2,58 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-slate-900 text-white">
+      <h1 className="text-2xl font-bold mb-6 text-yellow-400">
+        Course Management - Xaviers School for the Gifted
+      </h1>
+
+      <h2 className="text-xl mb-4 text-yellow-400">Add a Course</h2>
+      <div className="w-full max-w-md">
+        <form id="courseForm" className="flex flex-col gap-4">
+          <label className="text-sm font-semibold text-yellow-400">
+            Course Name:
+          </label>
+          <input
+            id="name"
+            placeholder="pleae provide the name of the course"
+            className="border border-yellow-400 p-2 rounded text-xl"
+            required
+          />
+          <label className="text-sm font-semibold text-yellow-400">
+            Course Description:
+          </label>
+          <textarea
+            id="description"
+            placeholder="please provde a course description"
+            className="border border-yellow-400 p-2 rounded text-xl"
+            required
+          ></textarea>
+          <label className="text-sm font-semibold text-yellow-400">
+            Subject Area:
+          </label>
+          <input
+            id="subject-area"
+            placeholder="subject Area of the course"
+            className="border border-yellow-400 p-2 rounded text-xl"
+            required
+          />
+          <label className="text-sm font-semibold text-yellow-400">
+            Credits
+          </label>
+          <input
+            id="credits"
+            type="number"
+            placeholder="provide the number of course credits"
+            className="border border-yellow-400 p-2 rounded text-xl"
+            required
+          />
+          <button className=" bg-yellow-400 text-black font-bold py-2 rounded-md hover:bg-yellow-300 transition ease-in delay-100 hover:translate-y-1">
+            Add Course
+          </button>
+        </form>
+      </div>
+      <h2 className="text-xl mt-8 text-yellow-400">All Courses</h2>
+      <div id="courses"></div>
+    </main>
   );
 }
